61954bc7d90a08820d3de380427da3743b7f85bd
RedBull4 <master.tatu.91@bk.ru>
2021-03-21 13:25:23 +0400

    Update cfg_manipulator

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "cfg_manipulator.hpp"
-#include <string>
-using namespace std;
 
 cfg_manipulator::cfg_file settings("settings.cfg");
 
 int main() {
-    for (size_t i = 0; i <= 100; i++)
-        settings.change_value("namespace", "line", to_string(i).c_str());
+    // Reads line "line".
+    CM_LOG(settings.read("line"));
+    // Reads line "line" in namesapce "namespace"
+    CM_LOG(settings.read("namespace", "line"));
+
+    // Changes value of the line "line"
+    settings.change_value("line", "value");
+    // Changes value of the line "line" in the namespace "namespace"
+    settings.change_value("namespace", "line", "value");
 }
