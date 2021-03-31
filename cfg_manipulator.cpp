3492980c61cdcc73a5fe1538f7052bcfa7f81d1c
Alibekov Alexandr <alexandralibekov@yahoo.com>
2021-03-31 22:06:09 +0400

    Update cfg_manipulator

--- cfg_manipulator.cpp
+++ cfg_manipulator.cpp
@@ -1,17 +1,20 @@
 #include "cm_includes.hpp"
 using namespace cfg_manipulator;
 
-namespace cfg_manipulator {
+namespace cfg_manipulator
+{
     FILE *file;
 
-    struct file_data {
+    struct file_data
+    {
         vector<pair<size_t, string>> lines;
         map<string, vector<pair<size_t, string>>> namespaces;
         CM_C_STRING file_path;
         bool errors = false;
     } file_data;
 
-    void print_error(string message, size_t line_id, bool _exit) {
+    void print_error(const string message, size_t line_id, bool _exit)
+    {
         if (line_id > 0)
             printf("\x1B[31mERROR\x1B[0m[cfg_manipulator]: Line %s\n",
                    string(to_string(line_id) + ", " + message).c_str());
@@ -27,11 +30,15 @@ namespace cfg_manipulator {
 
     char *standard_string() { return (char *)malloc(WCHAR_MAX); }
 
-    CM_C_STRING trim_characters(CM_C_STRING str, const size_t begin,
-                                const size_t end, CM_C_CHAR character) {
+    CM_C_STRING trim_characters(const CM_C_STRING str, const size_t begin,
+                                const size_t end, CM_C_CHAR character)
+    {
         CM_STRING output = standard_string();
 
-        for (size_t i = begin; i < end; i++) {
+        memset(output, 0, WCHAR_MAX);
+
+        for (size_t i = begin; i < end; i++)
+        {
             if (str[i] != character)
                 output[strlen(output)] = str[i];
         }
@@ -39,18 +46,21 @@ namespace cfg_manipulator {
         return output;
     }
 
-    bool is_namespace(const size_t line_id, CM_C_STRING line) {
+    bool is_namespace(const size_t line_id, const CM_C_STRING line)
+    {
         bool begin = false, end = false;
         CM_C_STRING _line = trim_characters(line, 0, strlen(line), ' ');
 
-        for (size_t i = 0; i < strlen(_line); i++) {
+        for (size_t i = 0; i < strlen(_line); i++)
+        {
             if (_line[i] == '[' && !end)
                 begin = true;
             if (_line[i] == ']')
                 end = true;
         }
 
-        if (begin && end) {
+        if (begin && end)
+        {
             if (_line[0] != '[' || _line[strlen(_line) - 1] != ']')
                 print_error("namespace contains undefined character.", line_id,
                             false);
@@ -59,12 +69,17 @@ namespace cfg_manipulator {
         return true ? begin && end : false;
     }
 
-    CM_C_STRING get_namespace_name(size_t line_id, CM_C_STRING line) {
+    CM_C_STRING get_namespace_name(size_t line_id, const CM_C_STRING line)
+    {
         CM_STRING output = standard_string();
         bool _begin = false, _bool = false;
 
-        for (size_t i = 0; i < strlen(line); i++) {
-            if (line[i] == '[') {
+        memset(output, 0, WCHAR_MAX);
+
+        for (size_t i = 0; i < strlen(line); i++)
+        {
+            if (line[i] == '[')
+            {
                 _begin = true;
                 continue;
             }
@@ -79,14 +94,17 @@ namespace cfg_manipulator {
                 output[strlen(output)] = line[i];
         }
 
-        for (size_t i = 0; i < strlen(output); i++) {
+        for (size_t i = 0; i < strlen(output); i++)
+        {
             if (output[(strlen(output) - 1) - i] != ' ')
                 break;
             output[(strlen(output) - 1) - i] = 0;
         }
 
-        for (size_t i = 0; i < strlen(output); i++) {
-            if (output[i] == ' ') {
+        for (size_t i = 0; i < strlen(output); i++)
+        {
+            if (output[i] == ' ')
+            {
                 print_error("spaces are not allowed in namespace names.",
                             line_id, false);
                 break;
@@ -96,11 +114,13 @@ namespace cfg_manipulator {
         return output;
     }
 
-    size_t get_characters_count(CM_C_STRING str, const size_t begin,
-                                const size_t end, CM_C_CHAR character) {
+    size_t get_characters_count(const CM_C_STRING str, const size_t begin,
+                                const size_t end, CM_C_CHAR character)
+    {
         size_t output = 0;
 
-        for (size_t i = begin; i < end; i++) {
+        for (size_t i = begin; i < end; i++)
+        {
             if (str[i] == character)
                 output++;
         }
@@ -108,12 +128,18 @@ namespace cfg_manipulator {
         return output;
     }
 
-    CM_C_STRING get_line_name(CM_C_STRING line) {
+    CM_C_STRING get_line_name(const CM_C_STRING line)
+    {
         CM_STRING _line = standard_string(), output = standard_string();
         bool _bool = false;
 
-        for (size_t i = 0; i < strlen(line); i++) {
-            if (line[i] == '=') {
+        memset(_line, 0, WCHAR_MAX);
+        memset(output, 0, WCHAR_MAX);
+
+        for (size_t i = 0; i < strlen(line); i++)
+        {
+            if (line[i] == '=')
+            {
                 _bool = false;
                 break;
             }
@@ -123,7 +149,8 @@ namespace cfg_manipulator {
                 _line[strlen(_line)] = line[i];
         }
 
-        for (size_t i = 0; i < strlen(_line); i++) {
+        for (size_t i = 0; i < strlen(_line); i++)
+        {
             if (_line[(strlen(_line) - 1) - i] != ' ')
                 _bool = true;
             if (_bool)
@@ -134,14 +161,17 @@ namespace cfg_manipulator {
         return output;
     }
 
-    void scan_line_for_errors(const size_t line_id, CM_C_STRING line) {
+    void scan_line_for_errors(const size_t line_id, const CM_C_STRING line)
+    {
         CM_C_STRING _line = trim_characters(line, 0, strlen(line), ' ');
         size_t characters[2] = {0, 0}, first_quote_id = 0;
 
-        for (size_t i = 0; i < strlen(_line); i++) {
+        for (size_t i = 0; i < strlen(_line); i++)
+        {
             if (_line[i] == '=' && characters[1] == 0)
                 characters[0]++;
-            if (_line[i] == '"') {
+            if (_line[i] == '"')
+            {
                 characters[1]++;
 
                 if (first_quote_id == 0)
@@ -151,7 +181,8 @@ namespace cfg_manipulator {
 
         if (strcmp(_line, "") != 0 &&
             get_characters_count(_line, 0, strlen(_line), ' ') !=
-                strlen(_line)) {
+                strlen(_line))
+        {
             if (characters[0] != 1 || characters[1] != 2)
                 print_error("the line shoud be in the following style: line = "
                             "\"value\".",
@@ -163,29 +194,38 @@ namespace cfg_manipulator {
                             false);
 
             if (_line[first_quote_id - 1] != '=' ||
-                _line[strlen(_line) - 1] != '"') {
+                _line[strlen(_line) - 1] != '"')
+            {
                 print_error("line contains undefined characters.", line_id,
                             false);
             }
         }
     }
 
-    void trim_comment(CM_STRING &line) {
+    void trim_comment(CM_STRING &line)
+    {
         CM_STRING output = standard_string();
         bool quote = false;
 
-        for (size_t i = 0; i < strlen(line); i++) {
-            if (!quote) {
+        memset(output, 0, WCHAR_MAX);
+
+        for (size_t i = 0; i < strlen(line); i++)
+        {
+            if (!quote)
+            {
                 if (line[i] == '#')
                     break;
             }
 
             output[strlen(output)] = line[i];
 
-            if (line[i] == '"' && !quote) {
+            if (line[i] == '"' && !quote)
+            {
                 quote = true;
                 continue;
-            } else if (line[i] == '"' && quote) {
+            }
+            else if (line[i] == '"' && quote)
+            {
                 quote = false;
                 continue;
             }
@@ -194,22 +234,28 @@ namespace cfg_manipulator {
         strcpy(line, output);
     }
 
-    void parse_file() {
+    void parse_file()
+    {
         CM_STRING line = standard_string(), namespace_name = standard_string();
         size_t line_id = 0;
         bool _namespace = false;
 
+        memset(line, 0, WCHAR_MAX);
+        memset(namespace_name, 0, WCHAR_MAX);
+
         file_data.lines.clear();
         file_data.namespaces.clear();
 
-        while (fgets(line, WCHAR_MAX, file)) {
+        while (fgets(line, WCHAR_MAX, file))
+        {
             line_id++;
 
             line[strcspn(line, "\n")] = 0;
 
             trim_comment(line);
 
-            if (is_namespace(line_id, line)) {
+            if (is_namespace(line_id, line))
+            {
                 if (!_namespace)
                     _namespace = true;
 
@@ -232,10 +278,14 @@ namespace cfg_manipulator {
         }
     }
 
-    CM_C_STRING get_file_type(CM_C_STRING file_path) {
+    CM_C_STRING get_file_type(const CM_C_STRING file_path)
+    {
         CM_STRING output = standard_string();
 
-        for (size_t i = 0; i < strlen(file_path); i++) {
+        memset(output, 0, 260);
+
+        for (size_t i = 0; i < strlen(file_path) - 1; i++)
+        {
             output[i] = file_path[(strlen(file_path) - 1) - i];
 
             if (file_path[(strlen(file_path) - 1) - i] == '.')
@@ -247,25 +297,36 @@ namespace cfg_manipulator {
         return output;
     }
 
-    void check_file_type(CM_C_STRING file_path) {
+    void check_file_type(const CM_C_STRING file_path)
+    {
         CM_STRING file_type = standard_string();
 
         strcpy(file_type, get_file_type(file_path));
 
-        for (size_t i = 0; i < CM_ARRAY_SIZE(allowed_file_types); i++) {
+        for (size_t i = 0; i < CM_ARRAY_SIZE(allowed_file_types); i++)
+        {
             if (strcmp(file_type, allowed_file_types[i]) == 0)
                 break;
             else
+            {
+                CM_LOG(file_type);
+
                 print_error("Unrecognized file type.", 0, true);
+            }
         }
     }
 
-    CM_C_STRING get_line_value(CM_C_STRING line) {
+    CM_C_STRING get_line_value(const CM_C_STRING line)
+    {
         CM_STRING output = standard_string();
         bool _begin = false;
 
-        for (size_t i = 0; i < strlen(line); i++) {
-            if (line[i] == '"' && !_begin) {
+        memset(output, 0, WCHAR_MAX);
+
+        for (size_t i = 0; i < strlen(line); i++)
+        {
+            if (line[i] == '"' && !_begin)
+            {
                 _begin = true;
                 continue;
             }
@@ -281,13 +342,16 @@ namespace cfg_manipulator {
 
 cfg_file::cfg_file() {}
 
-void cfg_file::open(CM_C_STRING file_path) {
+void cfg_file::open(const CM_C_STRING file_path)
+{
     check_file_type(file_path);
 
-    if (access(file_path, F_OK) == 0) {
+    if (access(file_path, F_OK) == 0)
+    {
         file = fopen(file_path, "r");
         file_data.file_path = file_path;
-    } else
+    }
+    else
         print_error("No such file or directory \""s + file_path + "\".", 0,
                     true);
 
@@ -299,18 +363,21 @@ void cfg_file::open(CM_C_STRING file_path) {
 
 bool cfg_file::is_open() { return file != NULL; }
 
-void cfg_file::close() {
+void cfg_file::close()
+{
     fclose(file);
     file = NULL;
 }
 
-CM_C_STRING cfg_file::read(CM_C_STRING line_name) {
+CM_C_STRING cfg_file::read(const CM_C_STRING line_name)
+{
     CM_STRING output = standard_string();
 
     if (!is_open())
         print_error("File is not open.", 0, true);
 
-    for (size_t i = 0; i < file_data.lines.size(); i++) {
+    for (size_t i = 0; i < file_data.lines.size(); i++)
+    {
         if (strcmp(get_line_name(file_data.lines.at(i).second.c_str()),
                    line_name) == 0)
             strcpy(output, file_data.lines.at(i).second.c_str());
@@ -323,15 +390,18 @@ CM_C_STRING cfg_file::read(CM_C_STRING line_name) {
     return get_line_value(output);
 }
 
-CM_C_STRING cfg_file::read(CM_C_STRING namespace_name, CM_C_STRING line_name) {
+CM_C_STRING cfg_file::read(const CM_C_STRING namespace_name, const CM_C_STRING line_name)
+{
     CM_STRING output = standard_string();
 
     if (!is_open())
         print_error("File is not open.", 0, true);
 
-    if (file_data.namespaces.count(namespace_name) != 0) {
+    if (file_data.namespaces.count(namespace_name) != 0)
+    {
         for (pair<size_t, string> line :
-             file_data.namespaces.at(namespace_name)) {
+             file_data.namespaces.at(namespace_name))
+        {
             if (strcmp(get_line_name(line.second.c_str()), line_name) == 0)
                 strcpy(output, line.second.c_str());
         }
@@ -341,29 +411,34 @@ CM_C_STRING cfg_file::read(CM_C_STRING namespace_name, CM_C_STRING line_name) {
                             "\" couldn't be found in the namespace \"" +
                             namespace_name + "\".",
                         0, true);
-    } else
+    }
+    else
         print_error("Unable to find namespace \""s + namespace_name + "\".", 0,
                     true);
 
     return get_line_value(output);
 }
 
-CM_C_STRING change_line_value(size_t line_id, CM_C_STRING line,
-                              CM_C_STRING value) {
+CM_C_STRING change_line_value(size_t line_id, const CM_C_STRING line,
+                              const CM_C_STRING value)
+{
     CM_STRING output = standard_string(), first = standard_string(),
               second = standard_string();
     size_t begin_quote_id = 0;
     bool _bool = false;
 
-    for (size_t i = 0; i < strlen(line); i++) {
+    for (size_t i = 0; i < strlen(line); i++)
+    {
         first[i] = line[i];
-        if (line[i] == '"') {
+        if (line[i] == '"')
+        {
             begin_quote_id = i + 1;
             break;
         }
     }
 
-    for (size_t i = begin_quote_id; i < strlen(line); i++) {
+    for (size_t i = begin_quote_id; i < strlen(line); i++)
+    {
         if (line[i] == '"')
             _bool = true;
         if (_bool)
@@ -380,14 +455,17 @@ CM_C_STRING change_line_value(size_t line_id, CM_C_STRING line,
     return output;
 }
 
-void cfg_file::change_value(CM_C_STRING line_name, CM_C_STRING value) {
+void cfg_file::change_value(const CM_C_STRING line_name, const CM_C_STRING value)
+{
     bool _bool = false;
 
     if (!is_open())
         print_error("File is not open.", 0, true);
 
-    for (pair<size_t, string> &line : file_data.lines) {
-        if (strcmp(line_name, get_line_name(line.second.c_str())) == 0) {
+    for (pair<size_t, string> &line : file_data.lines)
+    {
+        if (strcmp(line_name, get_line_name(line.second.c_str())) == 0)
+        {
             line.second =
                 change_line_value(line.first, line.second.c_str(), value);
             _bool = true;
@@ -401,23 +479,28 @@ void cfg_file::change_value(CM_C_STRING line_name, CM_C_STRING value) {
             0, true);
 }
 
-void cfg_file::change_value(CM_C_STRING namespace_name, CM_C_STRING line_name,
-                            CM_C_STRING value) {
+void cfg_file::change_value(const CM_C_STRING namespace_name, const CM_C_STRING line_name,
+                            const CM_C_STRING value)
+{
     bool _bool = false;
 
     if (!is_open())
         print_error("File is not open.", 0, true);
 
-    if (file_data.namespaces.count(namespace_name) != 0) {
+    if (file_data.namespaces.count(namespace_name) != 0)
+    {
         for (pair<size_t, string> &line :
-             file_data.namespaces.at(namespace_name)) {
-            if (strcmp(line_name, get_line_name(line.second.c_str())) == 0) {
+             file_data.namespaces.at(namespace_name))
+        {
+            if (strcmp(line_name, get_line_name(line.second.c_str())) == 0)
+            {
                 line.second =
                     change_line_value(line.first, line.second.c_str(), value);
                 _bool = true;
             }
         }
-    } else
+    }
+    else
         print_error("Unable to change the value of the line, because "
                     "the namespace \""s +
                         namespace_name + "\" was not found in the file.",
