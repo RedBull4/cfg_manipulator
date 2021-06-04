6b91ead0b161c8df0b135e561fe75b8ce36ffb7f
Alibekov Alexandr <alexandralibekov@yahoo.com>
2021-06-04 19:09:05 +0400

    Update cfg_manipulator

---
+++ tests/OpenGL/build/CMakeFiles/OpenGL.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/alexa/Desktop/sawd/glad.c" "CMakeFiles/OpenGL.dir/glad.c.obj" "gcc" "CMakeFiles/OpenGL.dir/glad.c.obj.d"
+  "C:/Users/alexa/Desktop/sawd/cfg_manipulator.cpp" "CMakeFiles/OpenGL.dir/cfg_manipulator.cpp.obj" "gcc" "CMakeFiles/OpenGL.dir/cfg_manipulator.cpp.obj.d"
+  "C:/Users/alexa/Desktop/sawd/main.cpp" "CMakeFiles/OpenGL.dir/main.cpp.obj" "gcc" "CMakeFiles/OpenGL.dir/main.cpp.obj.d"
+  "C:/Users/alexa/Desktop/sawd/shaderLoader.cpp" "CMakeFiles/OpenGL.dir/shaderLoader.cpp.obj" "gcc" "CMakeFiles/OpenGL.dir/shaderLoader.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
